2e244345cc31276007f72759bfa230fd84110809
riyazedd <riyaz.shrestha00@gmail.com>
2025-03-16 09:51:04 +0545

    update

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://swoyatnayonjan.com.np", // Adjust if needed
+    baseURL: "http://swoyatnayonjan.com.np", // Adjust if needed
     withCredentials: true, // Allow sending cookies
 });
 
